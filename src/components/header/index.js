11fc0e23567c92d641917c89622b9d31f6255d85
Grayson Gantek <graysonleegantek@gmail.com>
2022-06-03 20:59:17 -0400

    adds new live time component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,13 @@
-import { format } from 'date-fns'
-import { useState } from 'react'
+import LiveTime from '/src/components/live-time'
 import Search from '/src/components/search'
 import styles from './index.module.css'
 
 export default function Header() {
-  const [time, setTime] = useState(Date.now())
-
-  setInterval(function () {
-    setTime(Date.now())
-  }, 1000)
-
   return (
     <div className={styles.wrapper}>
       <div>
         <p>
-          <small>Today is {format(time, 'MMMM do, y')}</small>
+          <LiveTime />
         </p>
         <h1>Welcome to Embark</h1>
       </div>
---
+++ src/components/live-time/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+
+export default function LiveTime() {
+  const [time, setTime] = useState(null)
+  useEffect(() => {
+    setTime(Date.now())
+    const interval = setInterval(() => {
+      setTime(Date.now())
+    }, 1000)
+    return () => clearInterval(interval)
+  })
+
+  if (time)
+    return (
+      <small>
+        Today is {format(time, 'MMMM do, y')}. It's {format(time, 'h:m:saaa')}.
+      </small>
+    )
+}
