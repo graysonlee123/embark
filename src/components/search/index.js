9e4d19d0104abdc144e2408632a2a252e602cd1d
Grayson Gantek <graysonleegantek@gmail.com>
2022-06-03 21:21:48 -0400

    adds forward slash to search functionality

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react'
 import styles from './index.module.css'
 
 export default function Search() {
+  const [input, setInput] = useState('')
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => removeEventListener('keydown', handleKeyDown)
+  })
+
+  const handleKeyDown = (e) => {
+    if (e.key === '/') {
+      document.getElementById('search').focus()
+
+      if (document.activeElement !== e.target) {
+        e.preventDefault()
+      }
+    }
+  }
+
   return (
     <form
       className={styles.form}
@@ -8,9 +25,17 @@ export default function Search() {
       action="https://google.com/search"
     >
       <label htmlFor="search">
-        <small>Search Google</small>
+        <small>
+          Search by typing <code>/</code>
+        </small>
       </label>
-      <input id="search" type="text" name="q" />
+      <input
+        id="search"
+        type="text"
+        name="q"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
     </form>
   )
 }
